d48459e988255c0fd18c936c84890a51e5aa6911
Wob76 <quotidian@gmail.com>
2020-09-09 20:54:36 +1000

    Preheat and M600 Tweaks
    Enable Hot End Preheat Time
    
    Tweak Load Length for M600

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -328,7 +328,7 @@
 // hot end to reach the target temperature, but the time it takes to reach
 // the minimum temperature your thermistor can read. The lower the better/safer.
 // This shouldn't need to be more than 30 seconds (30000)
-//#define MILLISECONDS_PREHEAT_TIME 0
+#define MILLISECONDS_PREHEAT_TIME 5000 // *WOB* (E) (0)
 
 // @section extruder
 
@@ -2068,7 +2068,7 @@
                                                   // 0 to disable start loading and skip to fast load only
   #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE  30  // (mm/s) Load filament feedrate. This can be pretty fast. *WOB* (6)
   #define FILAMENT_CHANGE_FAST_LOAD_ACCEL     25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  #define FILAMENT_CHANGE_FAST_LOAD_LENGTH   405  // (mm) Load length of filament, from extruder gear to nozzle. *WOB* (0)
+  #define FILAMENT_CHANGE_FAST_LOAD_LENGTH   390  // (mm) Load length of filament, from extruder gear to nozzle. *WOB* (0)
                                                   //   For Bowden, the full length of the tube and nozzle.
                                                   //   For direct drive, the full length of the nozzle.
   //#define ADVANCED_PAUSE_CONTINUOUS_PURGE       // Purge continuously up to the purge length until interrupted.
